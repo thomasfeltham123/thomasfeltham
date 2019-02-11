be54d6703bd23417d4a3e9edb34f4f03b0620f7e
thomasfeltham123 <47329920+thomasfeltham123@users.noreply.github.com>
2019-02-11 11:00:37 -0600

    Primative paint.
    First project, Feb 11, 2019.

---
+++ ThomasFeltham PrimativePaint/sketch.js
@@ -0,0 +1,37 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+let x = mouseX;
+let y = mouseY;
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(0, 0, 0);
+}
+
+function draw() {
+  
+}
+
+keyTyped();
+
+function keyTyped(){
+  if(key === 'a'){
+  fill(random(1, 255), random(1, 255), random(1, 255));
+  rect(mouseX, mouseY, 55, 100);
+  }
+  else if(key === 's'){
+    fill(random(1, 255), random(1, 255), random(1, 255));
+    ellipse(mouseX, mouseY, 55, 55);
+  }
+  else if(key === 'd'){
+    fill(random(1, 255), random(1, 255), random(1, 255));
+    triangle(mouseX += 20, mouseY +=20 , mouseX -= 20, mouseY -= 20, mouseX +=20, mouseY -= 20);
+  }
+
+}
