583982bbf27462ed78c51bd590bfda995bab6206
thomasfeltham123 <47329920+thomasfeltham123@users.noreply.github.com>
2019-02-15 11:01:43 -0600

    Update sketch.js

--- ThomasFeltham PrimativePaint/sketch.js
+++ ThomasFeltham PrimativePaint/sketch.js
@@ -1,13 +1,13 @@
-// Project Title
-// Your Name
-// Date
+// Primative Paint
+// Thomas F.
+// February 15, 2019.
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let x;
-let y;
-let rectSize = 100;
+
+let rectSize = 50;
+let rectSpeed = -1;
 
 
 
@@ -20,43 +20,42 @@ function setup() {
 
 function draw() {
 
-  
-rect(windowWidth/2, windowHeight/2, rectSize, rectSize);
-if(rectSize <= 10){
-  rectSize *= 2;
-}
-else if(rectSize >= 150){
-  rectSize *= 5;
-}
+//This creates the moving rectangle.
+  rect(windowWidth/2, windowHeight/2, rectSize, rectSize);
+  if(rectSize > 100 || rectSize < 50){
+    rectSpeed *= -1;
+  }
+  rectSize += rectSpeed;
 
-
+//This creates the text and gives it color.
   fill(10, 100, 170);
   textSize(30);
   textFont('Times New Roman');
-  text("Thomas G. Feltham", windowWidth-250, windowHeight-10);
-
-
+  text("Thomas G. Feltham", windowWidth - 250, windowHeight - 10);
+  
 }
 
-keyTyped()
 
 
 
-function keyTyped(){
-  if(key === 'a'){
-  fill(random(1, 255), random(1, 255), random(1, 255));
-  rect(mouseX, mouseY, 55, 100);
+//This function makes all of the required letters to do their jobs.
+//A makes a rectangle, S makes a ellipse, D makes a triangle, and
+// SPACE Clears everything.
+function keyTyped() {
+  if (key === 'a') {
+    fill(random(1, 255), random(1, 255), random(1, 255));
+    rect(mouseX, mouseY, 55, 100);
   }
-  else if(key === 's'){
+  else if (key === 's') {
     fill(random(1, 255), random(1, 255), random(1, 255));
     ellipse(mouseX, mouseY, 55, 55);
   }
-  else if(key === 'd'){
+  else if (key === 'd') {
     fill(random(1, 255), random(1, 255), random(1, 255));
-    triangle(mouseX += 20, mouseY +=20 , mouseX -= 20, mouseY -= 20, mouseX +=20, mouseY -= 20);
+    triangle(mouseX += 20, mouseY += 20, mouseX -= 20, mouseY -= 20, mouseX += 20, mouseY -= 20);
   }
-else if(key === " "){
-   background(0);
+  else if (key === " ") {
+    background(0);
   }
 }
 
