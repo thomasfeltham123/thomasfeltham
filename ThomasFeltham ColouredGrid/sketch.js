82f61285df8f26ed21cfd8353f68a1911faa0f53
thomasfeltham123 <47329920+thomasfeltham123@users.noreply.github.com>
2019-03-12 10:56:54 -0600

    Update sketch.js

--- ThomasFeltham ColouredGrid/sketch.js
+++ ThomasFeltham ColouredGrid/sketch.js
@@ -3,30 +3,36 @@
 // March 7, 2019
 //
 // Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// - I made it so the colors are more aesthetric as they are only partially random.
+// - Ive also made it so the window is full of squares, none are cut off by the edges.
+
 
 let squareSize;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  squareSize = 10;
+  squareSize = 45;
 
   noLoop();
 }
 
 function draw() {
-
+background(0);
 drawGrid();
 
+
+}
+//This changes the value of windowWidth and windowHeight based on the dimensions of the window
+function windowResized(){
+resizeCanvas(windowWidth, windowHeight);
 }
 
 
-
-
+//This creates the very first grid, and every other one as the sketch refreshes.
 function drawGrid(){
-for(var x=0; x<=width; x+=width/10){
-  for(var y=0; y<=height; y+=height/10){
+for(let x=0; x<=width; x+=width/squareSize){
+  for(let y=0; y<=height; y+=height/squareSize){
     fill(int(random(255)), 100, 200);
     rect(x, y, width/squareSize, height/squareSize);
     }
@@ -37,15 +43,19 @@ function keyPressed(){
   draw();
 }
 
-
+//This controlls the size change based on which mouse button you press.
 function mousePressed(){
     if(mouseButton === LEFT){
-      squareSize += 5;
-      draw();
+      if(squareSize > 1){
+        squareSize += 5;
+        draw();
+      }
     }
     if(mouseButton === RIGHT){
-      squareSize -+ 5;
-      draw();
+      if(squareSize > 5){
+        squareSize -= 5;
+        draw();
+      }
     }
   }
   
