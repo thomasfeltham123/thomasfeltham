04d39958ff7e24a5243868fcdad5cca82ef5f40b
thomasfeltham123 <47329920+thomasfeltham123@users.noreply.github.com>
2019-03-19 10:24:51 -0600

    Update sketch.js

--- clock 11/sketch.js
+++ clock 11/sketch.js
@@ -1,5 +1,5 @@
-// Project Title
-// Your Name
+// Basic transformations clock
+// Thomas Feltham
 // Date
 //
 // Extra for Experts:
@@ -11,16 +11,33 @@ function setup() {
 }
 
 function draw() {
-  background(220);
-  drawClock();
+  background(20, 50, 190);
+  push();
+  translate(width/2, height/2);
+  strokeWeight(3);
+  ellipse(0, 0, 600, 600);
+
+  for(let i=0; i<360; i+=6){
+    push();
+    rotate(radians(i));
+    if(i % 30 === 0){
+    //Thick line
+      strokeWeight(5);
+      line(250, 0, 280, 0);
+    }else{
+    //thin line
+      strokeWeight(2);
+      line(260, 0, 280, 0);
+  }
+    pop();
+
+  }
+  push();
+  rotate(radians(frameCount/9));
+  strokeWeight(5);
+  line(0, 0, 200, 0);
+  pop();
+
+  pop();
 }
 
-
-
-function drawClock(){
-
-  ellipse(width/2, height/2, 200, 200);
-
-  
-
-}
