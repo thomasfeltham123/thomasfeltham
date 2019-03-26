844990b58dbbd0b7d54e48f04eaf827806808b88
thomasfeltham123 <47329920+thomasfeltham123@users.noreply.github.com>
2019-03-26 10:19:05 -0600

    Update sketch.js

--- Thomas Feltham Sprites/sketch.js
+++ Thomas Feltham Sprites/sketch.js
@@ -5,11 +5,41 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let spaceman = [];
+let speed = 20;
+let counter = 0;
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  imageMode(CENTER);
 }
 
-function draw() {
-  background(220);
+function preload(){
+  for(let i =1; i<9; i++){
+    spaceman.push(loadImage('assets/Sprite0' + i + '.png'));
+    print(i);
+  }
 }
+
+
+function draw() {
+  preload();
+  background(220);
+  image(spaceman[counter], width/2, height/2)
+  spriteCycle();
+
+}
+
+
+
+
+function spriteCycle(){
+  if (frameCount % int(speed) === 0){
+    counter ++;
+    if (counter > 8) counter = 0;
+  }
+}
+
+
+
