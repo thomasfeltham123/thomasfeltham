689d409f1888b054fde6f91bcaa6e4d78ba44a1e
thomasfeltham123 <47329920+thomasfeltham123@users.noreply.github.com>
2019-05-03 11:01:26 -0600

    Update sketch.js

--- puzzle game/sketch.js
+++ puzzle game/sketch.js
@@ -1,65 +1,82 @@
-// Project Title
-// Grid Demo
-const NUM_ROWS = 4;
-const NUM_COLS = 5;
+//Insert your Comment Header here.
+
+let NUM_ROWS = 4;
+let NUM_COLS = 5;
 let rectWidth, rectHeight;
 let currentRow, currentCol;
-
-let gridData = [[0,0,0,255,0],
-                [255,0,255,0,255],
+let gridData = [[0,0,0,0,0],
+                [0,0,0,0,0],
                 [0,255,0,0,0],
-                [0,0,255,0,255]];
+                [255,255,255,0,0]];
+
+
 
 function setup() {
+  // Determine the size of each square. Could use windowHeight,windowHeight  for Canvas to keep a square aspect ratio
   createCanvas(windowWidth, windowHeight);
-  rectWidth = width / NUM_COLS;
-  rectHeight = height / NUM_ROWS;
+  rectWidth = width/NUM_COLS;
+  rectHeight = height/NUM_ROWS;
 }
 
-function drawGrid(){
-  // Render a Grid of Squares - fill color set 
-  // According to data stored in 2D array.
-  for (let y = 0; y < NUM_ROWS; y++){
-    for (let x = 0; x < NUM_COLS; x++){
-      fill(gridData[y][x]);
-      rect(x*rectWidth,y*rectHeight,rectWidth,rectHeight);
+function draw() {
+  background(220);
+  determineActiveSquare();   //figure out which tile the mouse cursor is over
+  drawGrid();                //render the current game board to the screen (and the overlay)
+  //didYouWin()
+}
+
+
+
+function mousePressed(){
+  // cross-shaped pattern flips on a mouseclick. Boundary conditions are checked within the flip function to ensure in-bounds access for array
+  if(mouseButton === LEFT){
+  flip(currentCol, currentRow);
+  flip(currentCol-1, currentRow);
+  flip(currentCol+1, currentRow);
+  flip(currentCol, currentRow-1);
+  flip(currentCol, currentRow+1);
+  }
+
+  if(mouseButton === CENTER){
+    flip(currentCol, currentRow);
+  }
+}
+
+function flip(col, row){
+  // given a column and row for the 2D array, flip its value from 0 to 255 or 255 to 0
+  // conditions ensure that the col and row given are valid and exist for the array. If not, no operations take place.
+  if (col >= 0 && col < NUM_COLS ){
+    if (row >= 0 && row < NUM_ROWS){
+      if (gridData[row][col] === 0) gridData[row][col] = 255;
+      else gridData[row][col] = 0;
     }
   }
 }
 
 function determineActiveSquare(){
-  //An expression to run each frame and to determine
-  //where the mouse is!
+  // An expression to run each frame to determine where the mouse currently is.
   currentRow = int(mouseY / rectHeight);
   currentCol = int(mouseX / rectWidth);
-  print(currentCol, currentRow);
-}
-function mousePressed(){
-
-  flip(currentCol, currentRow );//mouse
-  flip(currentCol-1, currentRow );//left
-  flip(currentCol+1, currentRow );//right
-  flip(currentCol, currentRow-1 );//up
-  flip(currentCol, currentRow+1 );//down
-
-  if(keyCode === RIGHT_ARROW){
-    flip(currentCol, currentRow );
-  }
 }
 
-function flip(col, row){
-  if (col >= 0 && col < NUM_COLS && row >= 0 && row < NUM_ROWS){
-    if (gridData[row][col] === 0){
-      gridData[row][col] = 255;
-    }
-    else{ 
-      gridData[row][col] = 0;
+function drawGrid(){
+  // Render a grid of squares - fill color set according to data stored in the 2D array
+  for (let x = 0; x < NUM_COLS ; x++){
+    for (let y = 0; y < NUM_ROWS; y++){
+      fill(gridData[y][x]); 
+      rect(x*rectWidth, y*rectHeight, rectWidth, rectHeight);
     }
   }
 }
 
-function draw() {
-  background(220);
-  determineActiveSquare();
-  drawGrid();
+function didYouWin(){
+  for(let i = 0; i < NUM_COLS; i++){
+    if(gridData[i][i] === 0 ){
+      textSize(40);
+      text('You Win!', Width/2, Height/2);
+      fill(100, 100, 100);
+
+    }
+  }
 }
+
