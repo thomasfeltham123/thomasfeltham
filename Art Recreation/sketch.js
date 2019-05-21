decd45256e44fc8c95aa5d1860bc099e6d760e36
thomasfeltham123 <47329920+thomasfeltham123@users.noreply.github.com>
2019-05-21 10:27:51 -0600

    Update sketch.js

--- Art Recreation/sketch.js
+++ Art Recreation/sketch.js
@@ -1,50 +1,33 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+let x1=0;
+let y1=0;
 
-let lineX = 650;
-let lineY = 700;
-let x = -100;
-let y = -100;
-
-
-
-function setup() {
+function setup(){
   createCanvas(windowWidth, windowHeight);
   noLoop();
 }
 
-function draw() {
+function draw(){
   background(220);
-
-  drawArt();
+  makeArt();
 }
 
-
-
-function drawArt(){
-
-
-  for(let i = 1; i <= 70; i ++){
-
-    if(x >= width/2){//Right Side
-      x = random(width/4, width/2);
-    }
-
-    if(x <= width/2){//Left Side
-      x = random(width/2 * 1.5, 100);
-    }
-
-    //x = random(200, 600);
-    y = random(1, 700);
-
-    line(lineX, lineY, x, y);
-    
-    lineX = x;
-    lineY = y;
+function makeArt(){
+  for(i = 0; i < 80; i++){
+    horizontalLines();
+    verticleLines();
   }
+}
 
+function horizontalLines(){
+  let x2 = random(width);
+
+  line(x1, y1, x2, y1);
+  x1 = x2;
+}
+
+function verticleLines(){
+  let y2 = random(height);
+
+  line(x1, y1, x1, y2);
+  y1 = y2;
 }
