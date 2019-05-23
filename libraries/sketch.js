b30423724cbee21c5a8137c1283133164a4b342a
thomasfeltham123 <47329920+thomasfeltham123@users.noreply.github.com>
2019-05-23 10:48:23 -0600

    Libraries Demo
    may 23, 2019

---
+++ libraries/sketch.js
@@ -0,0 +1,26 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+let scribble;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  scribble = new Scribble;
+}
+
+
+
+function draw() {
+  background(220);
+  randomSeed(0);
+  let x = [mouseX - 100, mouseX +100, mouseX + 100, mouseX - 100];
+  let y = [mouseY - 100, mouseY -100, mouseY + 100, mouseY + 100];
+  scribble.scribbleFilling(x, y, 4, 40);
+
+  scribble.scribbleRoundedRect(mouseX, mouseY, 200, 200, 10);
+}
