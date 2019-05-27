f8045c0173b3f2c9f7cdea5dc4fcd633c0e9c73c
thomasfeltham123 <47329920+thomasfeltham123@users.noreply.github.com>
2019-05-27 11:00:58 -0600

    Final Project Prep
    may 27, 2019

---
+++ Final Project Practice/sketch.js
@@ -0,0 +1,28 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background(200);
+  //backDrop();
+  drawSprites();
+}
+
+
+
+function backDrop(){
+  background(0, 150, 200);
+
+
+}
+
+
