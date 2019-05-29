85a852b30d763b91c3b8852d946a3308be57fa32
thomasfeltham123 <47329920+thomasfeltham123@users.noreply.github.com>
2019-05-29 11:00:38 -0600

    Update sketch.js

--- Final Project Practice/sketch.js
+++ Final Project Practice/sketch.js
@@ -5,24 +5,44 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let ground;
+let mario;
+
+function preload(){
+  //Loads images needed to animate Mario running forward.
+  let forwardRun = loadAnimation('assets/Forward01.png' , 'assets/Forward03.png');
+  mario = createSprite(0, 0, 100, 100);
+  mario.addAnimation('forward', forwardRun);
+
+//Loads the image of the grass from Mario
+  groundImg = loadImage('assets/Ground05.png');
+  ground = createSprite(10,10);
+  ground.addImage(groundImg);
+
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  createSprite(400, 200, 50, 50);
 }
 
 function draw() {
   background(200);
-  //backDrop();
+  makeSprites();
+}
+
+function makeSprites(){
+  push(); 
+  translate(width/2, height/2)
+  scale(2);
   drawSprites();
+  pop();
 }
 
+// function moving(){
+//   if(key === 'LEFT_ARROW'){
 
+//   }
+// }
 
-function backDrop(){
-  background(0, 150, 200);
-
-
-}
 
 
